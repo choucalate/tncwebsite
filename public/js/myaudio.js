9e9edfcae34fbf05aceb1a957f571b1761fec7ac
John Chou <choucalate@johnnays-mbp.attlocal.net>
2015-06-01 18:29:08 -0700

    another fix

--- public/js/myaudio.js
+++ public/js/myaudio.js
@@ -93,7 +93,7 @@ $(document).ready(function() {
 		console.log("playing at offset: " + offset);
 		setTimeout(function() {
 			
-			name = "audio/" + name;
+			name = "audio/" + name + ".ogg";
 			console.log("going to play: " + name + " index: " + index);
 			var snd1  = new Audio();
 			var src1  = document.createElement("source");
